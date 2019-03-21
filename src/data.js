6a240cfa5ecf2b784e70bfae3619357714113788
OlgaContreras911 <rockersita.911@gmail.com>
2019-03-20 18:53:34 -0600

    agregando orden en la impresión de muro

--- src/data.js
+++ src/data.js
@@ -1,44 +1,54 @@
-const postStatus = document.getElementById("post-status");
-const postButton = document.getElementById("post-button");
-const printPost = document.getElementById("print-post");
-var db = firebase.firestore();
-const docRef = firestore.collection("wallPost").doc("post");
-// const textToPost = postStatus.valuef;
-const deletePost = document.getElementById("delete-post");
-const editPost = document.getElementById("edit-post");
+// CONSTANTES
 
-function onloadWall() {
+const postStatus       = document.getElementById("post-status");
+const postButton       = document.getElementById("post-button");
+const printPost        = document.getElementById("print-post");
+const deletePost       = document.getElementById("delete-post");
+const editPost         = document.getElementById("edit-post");
 
+const db               = firebase.firestore();
+
+// INICIALIZADOR
+const onloadWall = () => {
   const bd = firebase.firestore();
-  const publicaciones = bd.collection('/wallPost').orderBy('name');
-  publicaciones.get().then(snapshot => {
-    snapshot.forEach(doc => {
-      // console.log(doc.id, '=>', doc.data());
-      if (user.id === doc.data().uid) {
+  const publicaciones = bd.collection('/wallPost');
+
+  publicaciones.get()
+  .then(snapshot => {
+    snapshot.forEach(async (doc) => {
+      const data      = await doc.data()
+      const user      = await firebase.auth().currentUser.uid
+      const dataUID   = data.uid
+      const dataName  = data.name.toUpperCase()
+      console.log(dataName)
+
+      if (user === dataUID) {
         document.getElementById("wall").innerHTML += `     
             <div class="col s12 m7">
-              <h4 class="header name-title">${doc.data().name.toUpperCase()}</h4>
+              <h4 class="header name-title">${data.name}</h4>
                 <div class="card horizontal z-depth-3">
                   <div class="c-i">
-                    <img class="user-photo"src="${doc.data().photo}">
+                    <img class="user-photo"src="${data.photo}">
                   </div>
                 <div class="card-stacked">
                   <div class="post">
-                    <p class="p-post">${doc.data().post}</p>
+                    <p class="p-post">${data.post}</p>
                   </div>
                 </div>
               </div>
+              <button onclick="editPost()" id="edit-post">Editar</button>
+              <button onclick="deletePost() "id="delete-post">Borrar</button>
             </div>`
           } else {
             `<div class="col s12 m7">
-          <h4 class="header name-title">${doc.data().name.toUpperCase()}</h4>
+          <h4 class="header name-title">${dataName}</h4>
             <div class="card horizontal z-depth-3">
               <div class="c-i">
-                <img class="user-photo"src="${doc.data().photo}">
+                <img class="user-photo"src="${data.photo}">
               </div>
             <div class="card-stacked">
               <div class="post">
-                <p class="p-post">${doc.data().post}</p>
+                <p class="p-post">${data.post}</p>
               </div>
             </div>
           </div>
@@ -48,63 +58,31 @@ function onloadWall() {
           }
     });
   });
-
 }
-console.log(new Date())
 
+// EVENTOS - Listeners
 postButton.addEventListener("click", () => {
   const muro = document.getElementById("wall")
   muro.innerHTML = ''
   let textToPost = postStatus.value;
-
-  textToPost;
-  console.log("guardando esto " + textToPost + " en FireStore");
-
-  docRef
-    .set({
-      status: textToPost
-    })
-    .then(() => {
-      console.log("gusrdando estado");
-    })
-    .catch(error => {
-      console.log("Hay un error:", error);
-    });
-  docRef
-    .get()
-    .then(doc => {
-      if (doc && doc.exists) {
-        const postData = doc.data();
-        printPost.value = postData.status;
-      }
-    })
-    .then(() => {
-      console.log("guardando impresion");
-    })
-    .catch(error => {
-      //   console.log("Hay un error en print:", error);
-    });
-
-  var provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  
   firebase.auth().onAuthStateChanged(async function (user) {
-
     if (user) {
-      console.log(user);
-
       await db.collection("wallPost")
         .add({
           name: user.displayName,
           email: user.email,
           post: textToPost,
           photo: user.photoURL,
-          uid: user.uid
-          //date: new Date,
+          uid: user.uid,
+          created_at: firebase.firestore.Timestamp.fromDate(new Date())
         })
 
       const bd = await firebase.firestore();
 
-      const postPublications = await bd.collection('/wallPost').orderBy('name');
+      const postPublications = await bd.collection('/wallPost')
+      .orderBy('created_at', "desc")
+      
       postPublications.get().then(snapshot => {
         snapshot.forEach(doc => {
           // console.log(doc.id, '=>', doc.data());
@@ -143,21 +121,9 @@ postButton.addEventListener("click", () => {
           }
         });
       });
-
     } else {
       console.log("No hay usuario loggeado")
     }
   });
     postStatus.value = '';  
 });
-
-// realTimeUpdates = () =>{
-//   docRef.onSnapshot(doc => {
-//     if (doc && doc.exists) {
-//       const postData = doc.data();
-//       printPost.value = postData.status;
-//     }
-//   })
-//   }
-
-//   realTimeUpdates();
